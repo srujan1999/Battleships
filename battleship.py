dce99fb9ec7a3b6cb557ad5c77e8b43af1f14509
srujan1999 <mylaramsrujankumar@msitprogram.net>
2021-12-21 10:46:28 +0530

    isVertical

--- battleship.py
+++ battleship.py
@@ -150,7 +150,13 @@ Parameters: 2D list of ints
 Returns: bool
 '''
 def isVertical(ship):
-    return
+    i=0
+    if ship[i][1]==ship[i+1][1]==ship[i+2][1]:
+        ship.sort()
+        if ship[i+1][0]-ship[i][0]==1 and ship[i+2][0]-ship[i+1][0]==1:
+            return True
+    return False
+        
 
 
 '''
@@ -309,7 +315,7 @@ def runSimulation(w, h):
 
 # This code runs the test cases to check your work
 if __name__ == "__main__":
-    test.testGrid()
+    test.testIsVertical()
 
     ## Finally, run the simulation to test it manually ##
-    runSimulation(500, 500)
+    #runSimulation(500, 500)
